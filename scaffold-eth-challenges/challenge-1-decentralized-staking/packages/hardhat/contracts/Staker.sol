92c79b3b3964a10829e38d8b04923d8b0bc736d9
jack ning <github@bytedesk.com>
2022-04-25 09:42:52 +0800

    update

---
+++ scaffold-eth-challenges/challenge-1-decentralized-staking/packages/hardhat/contracts/Staker.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "hardhat/console.sol";
+import "./ExampleExternalContract.sol";
+
+contract Staker {
+
+  ExampleExternalContract public exampleExternalContract;
+
+  constructor(address exampleExternalContractAddress) {
+      exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+  }
+
+  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
+  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  mapping ( address => uint256 ) public balances;
+
+  // After some `deadline` allow anyone to call an `execute()` function
+  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+  uint256 public deadline = block.timestamp + 30 seconds;
+
+  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+  uint256 public constant threshold = 1 ether;
+
+
+  // Add a `withdraw()` function to let users withdraw their balance
+
+
+  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+
+
+  // Add the `receive()` special function that receives eth and calls stake()
+
+
+}
