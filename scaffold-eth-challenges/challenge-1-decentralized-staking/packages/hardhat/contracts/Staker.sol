3772379ad9dfb6ce3efaf2559d6e7b614d110ab1
jack ning <github@bytedesk.com>
2022-04-26 11:16:05 +0800

    update

--- scaffold-eth-challenges/challenge-1-decentralized-staking/packages/hardhat/contracts/Staker.sol
+++ scaffold-eth-challenges/challenge-1-decentralized-staking/packages/hardhat/contracts/Staker.sol
@@ -23,14 +23,66 @@ contract Staker {
   // if the `threshold` was not met, allow everyone to call a `withdraw()` function
   uint256 public constant threshold = 1 ether;
 
+  // 
+  event Stake(address indexed sender, uint256 amount);
+
+  // 
+  modifier deadlineReached(bool requireReached) {
+    uint256 timeRemaing = timeLeft();
+    if (requireReached) {
+      require(timeRemaing == 0, "dealine is not reached");
+    } else {
+      require(timeRemaing > 0, "dealine is already reached");
+    }
+    _;
+  }
+
+  // 
+  modifier stakeNotCompleted() {
+    bool completed = exampleExternalContract.completed();
+    require(!completed, "staking already completed");
+    _;
+  }
+
+  function execute() public stakeNotCompleted deadlineReached(false) {
+    uint256 contractBalance = address(this).balance;
+    //
+    require(contractBalance >= threshold, "threshold not reached");
+    //
+    (bool sent,) = address(exampleExternalContract).call{value: contractBalance}(abi.encodeWithSignature("complete()"));
+    require(sent, "exampleExternalContract.complete failed");
+  }
+
+  // 
+  function stake() public payable {
+    // 
+    balances[msg.sender] += msg.value;
+    // 
+    emit Stake(msg.sender, msg.value);
+  }
 
   // Add a `withdraw()` function to let users withdraw their balance
-
+  function withdraw() public deadlineReached(true) stakeNotCompleted {
+    uint256 userBalance = balances[msg.sender];
+    //
+    require(userBalance > 0, "you don't have enough balance to withdraw");
+    // 
+    balances[msg.sender] = 0;
+    //
+    (bool sent, ) = msg.sender.call{value: userBalance}("");
+    require(sent, "failed to send user balance back to the user");
+  }
 
   // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
+  function timeLeft() public view returns (uint256 left) {
+    if (block.timestamp >= deadline) {
+      return 0;
+    } else {
+      return deadline - block.timestamp;
+    }
+  }
 
   // Add the `receive()` special function that receives eth and calls stake()
-
+  // function receive() public payable {}
 
 }
---
+++ solidity-by-example/readme.md
@@ -0,0 +1,3 @@
+# 手动输入代码-学习solidity
+
+- <https://solidity-by-example.org/>
