6fec238b4f65720c53a078ea129dd120ce993d32
jack ning <github@bytedesk.com>
2022-05-04 18:54:30 +0800

    Update IterableMapping.sol

--- solidity-by-example/contracts/IterableMapping.sol
+++ solidity-by-example/contracts/IterableMapping.sol
@@ -3,7 +3,16 @@
 pragma solidity ^0.8.10;
 
 library IterableMapping {
+    // Iterable mapping from address to uint;
+    struct Map {
+        address[] keys;
+        mapping(address => uint) values;
+        mapping(address => uint) indexOf;
+        mapping(address => bool) inserted;
+    }
+
     
+
 }
 
 contract TestIterableMap {
