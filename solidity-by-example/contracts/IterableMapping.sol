7fe714c589985661f0ad43661527b5c667703c2f
jack ning <github@bytedesk.com>
2022-05-04 11:57:24 +0800

    update

---
+++ solidity-by-example/contracts/IterableMapping.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// https://solidity-by-example.org/app/iterable-mapping
+pragma solidity ^0.8.10;
+
+library IterableMapping {
+    
+}
+
+contract TestIterableMap {
+
+}
