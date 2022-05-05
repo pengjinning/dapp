5d5cc744c33777a7e6919f6b86282a8aa2db8ab6
jack ning <github@bytedesk.com>
2022-05-05 11:29:07 +0800

    update

---
+++ solidity-by-example/contracts/EtherDelta.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+// https://etherdelta.com/#PPT-ETH
+pragma solidity ^0.8.10;
+
+/**
+You just created an Ethereum account: 0x8baeADb5110Cd1ACaADe8fF8B61ec5D36bb3c0A4
+Please BACKUP the private key for this account: 5da2e3e62d4224c764853afb687a60f7454bc6fda444fc7966ba13c4ba731e32
+ */
--- solidity-by-example/contracts/TokenSwap.sol
+++ solidity-by-example/contracts/TokenSwap.sol
@@ -2,92 +2,65 @@
 // https://solidity-by-example.org/app/erc20
 pragma solidity ^0.8.10;
 
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contracts/token/ERC20/IERC20.sol
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/IERC20.sol";
 
-    function balanceOf(address account) external view returns (uint256);
+/*
+How to swap tokens
 
-    function transfer(address recipient, uint256 amount)
-        external
-        returns (bool);
-
-    function allowance(address owner, address spender)
-        external
-        view
-        returns (uint256);
-
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approve(
-        address indexed sender,
-        address indexed recipient,
-        uint256 value
-    );
-}
-
-contract ERC20 is IERC20 {
-    uint256 public totalSupply;
-    mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
-    string public name = "Solidity By Example";
-    string public symbol = "SOLBYEX";
-    uint8 public decimals = 18;
-
-    function transfer(address recipient, uint256 amount)
-        external
-        returns (bool)
-    {
-        balanceOf[msg.sender] -= amount;
-        balanceOf[recipient] += amount;
-        emit Transfer(msg.sender, recipient, amount);
-        return true;
-    }
-
-    function approve(address spender, uint256 amount) external returns (bool) {
-        allowance[msg.sender][spender] = amount;
-        emit Approve(msg.sender, spender, amount);
-        return true;
-    }
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool) {
-        allowance[msg.sender][sender] -= amount;
-        balanceOf[sender] -= amount;
-        balanceOf[recipient] += amount;
-        emit Transfer(sender, recipient, amount);
-        return true;
-    }
-
-    function mint(uint amount) external returns (bool) {
-        balanceOf[msg.sender] += amount;
-        totalSupply += amount;
-        emit Transfer(address(0), msg.sender, amount);
-        return true;
-    }
-
-    function burn(uint amount) external returns (bool) {
-        balanceOf[msg.sender] -= amount;
-        totalSupply -= amount;
-        emit Transfer(msg.sender, address(0), amount);
-        return true;
-    }
-}
-
-contract MyToken is ERC20 {
-    
-}
-
+1. Alice has 100 tokens from AliceCoin, which is a ERC20 token.
+2. Bob has 100 tokens from BobCoin, which is also a ERC20 token.
+3. Alice and Bob wants to trade 10 AliceCoin for 20 BobCoin.
+4. Alice or Bob deploys TokenSwap
+5. Alice approves TokenSwap to withdraw 10 tokens from AliceCoin
+6. Bob approves TokenSwap to withdraw 20 tokens from BobCoin
+7. Alice or Bob calls TokenSwap.swap()
+8. Alice and Bob traded tokens successfully.
+*/
 contract TokenSwap {
+
+    IERC20 public token1;
+    address public owner1;
+    uint public amount1;
+    IERC20 public token2;
+    address public owner2;
+    uint public amount2;
+
+    constructor(
+        address _token1,
+        address _owner1,
+        uint _amount1,
+        address _token2,
+        address _owner2,
+        uint _amount2
+    ) {
+        token1 = IERC20(_token1);
+        owner1 = _owner1;
+        amount1 = _amount1;
+        token2 = IERC20(_token2);
+        owner2 = _owner2;
+        amount2 = _amount2;
+    }
+
+    function swap() public {
+        require(msg.sender == owner1 || msg.sender == owner2, "no auth");
+        require(
+            token1.allowance(ower1, address(this)) >= amount1, "token1 allowance too low"
+        );
+        require(
+            token2.allowance(owner2, address(this)) >= amount2, "token2 allowance too low"
+        );
+        _safeTransferFrom(token1, owner1, owner2, amount1);
+        _safeTransferFrom(token2, owner2, owner1, amount2);
+    }
+
+    function _safeTransferFrom(
+        IERC20 token,
+        address from,
+        address to,
+        uint amount
+    ) private {
+        bool success = token.transferFrom(from, to, amount);
+        require(success, "transfor failed");
+    }
 
 }
