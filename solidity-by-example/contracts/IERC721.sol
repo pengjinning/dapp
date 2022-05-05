92c445140f27ef4df864cf2f8de9504eab8d07b9
jack ning <github@bytedesk.com>
2022-05-05 15:35:17 +0800

    Update IERC721.sol

--- solidity-by-example/contracts/IERC721.sol
+++ solidity-by-example/contracts/IERC721.sol
@@ -6,35 +6,165 @@ interface IERC165 {
 }
 
 interface IERC721 is IERC165 {
-    function balanceOf(address owner) external view returns (uint balance);
+    function balanceOf(address owner) external view returns (uint256 balance);
 
-    function ownerOf(uint tokenId) external view returns (address owner);
+    function ownerOf(uint256 tokenId) external view returns (address owner);
 
     function safeTransferFrom(
         address from,
         address to,
-        uint tokenId
+        uint256 tokenId
     ) external;
 
     function safeTransferFrom(
         address from,
         address to,
-        uint tokenId,
+        uint256 tokenId,
         bytes calldata data
     ) external;
 
     function transferFrom(
         address from,
         address to,
-        uint tokenId
+        uint256 tokenId
     ) external;
 
-    function approve(address to, uint tokenId) external;
+    function approve(address to, uint256 tokenId) external;
 
-    function getApproved(uint tokenId) external view returns (address operator);
+    function getApproved(uint256 tokenId)
+        external
+        view
+        returns (address operator);
 
     function setApprovalForAll(address operator, bool _approved) external;
 
-    function isApprovedForAll(address owner, address operator) external view returns (bool);
-    
+    function isApprovedForAll(address owner, address operator)
+        external
+        view
+        returns (bool);
+}
+
+interface IERC721Receiver {
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4);
+}
+
+contract ERC721 is IERC721 {
+    using Address for address;
+
+    event Transfer(address indexed from, address indexed to, uint256 tokenId);
+    event Approval(
+        address indexed owner,
+        address indexed approved,
+        uint256 indexed tokenId
+    );
+    event ApprovalForAll(
+        address indexed owner,
+        address indexed operator,
+        bool approved
+    );
+
+    // Mapping from token ID to owner address
+    mapping(uint256 => address) private _owners;
+
+    // Mapping owner address to token count
+    mapping(address => uint256) private _balances;
+
+    // Mapping from token ID to approved address
+    mapping(uint256 => address) private _tokenApprovals;
+
+    // Mapping from owner to operator approvals
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+    function supportsInterface(bytes4 interfaceId)
+        external
+        pure
+        override
+        returns (bool)
+    {
+        return
+            interfaceId == type(IERC721).interfaceId ||
+            interfaceId == type(IERC165).interfaceId;
+    }
+
+    function balanceOf(address owner) external view returns (uint256) {
+        require(owner != address(0), "owner == zero address");
+        return _balances[owner];
+    }
+
+    function ownerOf(uint256 tokenId)
+        public
+        view
+        override
+        returns (address owner)
+    {
+        owner = _owners[tokenId];
+        require(owner != address(0), "token not exists");
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external {
+
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes calldata data
+    ) external {
+
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external {
+
+    }
+
+    function approve(address to, uint256 tokenId) external {
+
+    }
+
+    function getApproved(uint256 tokenId)
+        external
+        view
+        override
+        returns (address operator)
+    {
+        
+    }
+
+    function setApprovalForAll(address operator, bool approved) external override {
+        _operatorApprovals[msg.sender][operator] = approved;
+        emit ApprovalForAll(msg.sender, operator, approved);
+    }
+
+    function isApprovedForAll(address owner, address operator)
+        external
+        view
+        override
+        returns (bool)
+    {
+        return _operatorApprovals[owner][operator];
+    }
+}
+
+library Address {
+    function isContract(address account) internal view returns (bool) {
+        uint256 size;
+        assembly {
+            size := extcodesize(account)
+        }
+        return size > 0;
+    }
 }
