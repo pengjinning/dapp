697f78227a4053a506b41309477691b48ca7954b
jack ning <github@bytedesk.com>
2022-05-07 16:50:02 +0800

    update

---
+++ solidity-by-example/contracts/hacks/Overflow.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+// https://solidity-by-example.org/hacks/overflow
+// pragma solidity ^0.8.10;
+pragma solidity ^0.7.6;
+
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+
+contract TimeLock {
+
+    // using SafeMath for uint;
+    
+
+
+
+}
